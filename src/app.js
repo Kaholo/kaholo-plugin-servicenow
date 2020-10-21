14b7afcde75d158e1eef80e05eed64d91ee1d919
ilanyaniv <ilan.yaniv@gmail.com>
2020-10-21 23:42:00 +0300

    Initial commit for service now plugin

---
+++ src/app.js
@@ -0,0 +1,50 @@
+const fetch = require('node-fetch');
+const btoa = require('btoa')
+
+async function createIncident (action, settings) {
+    /**
+     * Set status of version to released inside a given project.
+     */
+    var bodyData = action.params.BODY;
+    if(typeof action.params.BODY != 'string'){
+        bodyData = JSON.stringify(action.params.BODY)
+    }
+    const method = "POST"
+    return await genericRestAPI(action,settings, bodyData,method)
+}
+
+async function genericRestAPI(action,settings, bodyData, method) {
+    /**
+     * Send Default API Request
+     */    
+    
+    const host = action.params.URL + "/api/now/table/incident";
+    const response = await fetch(host,  {
+        method: `${method}`,
+        headers: {
+          'Authorization': 'Basic '+btoa(`${action.params.USER}:${action.params.PASSWORD}`),
+             'Accept': 'application/json',
+              'Content-Type': 'application/json'
+        },
+        body: bodyData
+    })
+    if ( response.status < 200 || response.status > 299) {
+        throw response
+    }
+    return JSON.parse(await response.text());
+}
+
+var action = {
+    params : {
+        URL: "https://dev74899.service-now.com",
+        USER: "admin",
+        PASSWORD: "UbEfmxC54vLL",
+        BODY: "{\"active\":\"true\",\"short_description\":\"created by Kaholo Plugin\",\"caller_id\":\"John Snow\"}"
+    }
+}
+createIncident(action)
+module.exports = {
+    createIncident: createIncident
+}
+
+
